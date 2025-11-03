ded87fa450f27b575531b4d95c414ab5c8f8f9be
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2025-11-03 12:43:30 +0700

    test with questions

--- src/funcs.py
+++ src/funcs.py
@@ -1,3 +1,6 @@
+from multiprocessing.managers import Value
+
+
 def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
     """Функция вычисляет стоимость товаров с учётом налога."""
 
@@ -13,3 +16,12 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
         taxed_prices.append(price + tax)
 
     return taxed_prices
+
+
+def calculate_tax(price: float, tax_rate: float) -> float:
+    if price <= 0:
+        raise ValueError('Неверная цена')
+    if not 0 < tax_rate < 100:
+        raise ValueError('Неверный налоговый процент')
+    result = price * (100 + tax_rate) / 100
+    return result
--- tests/test_taxes.py
+++ tests/test_taxes.py
@@ -1,5 +1,6 @@
 import pytest
 from src.funcs import calculate_taxes
+from src.funcs import calculate_tax
 
 
 @pytest.fixture
@@ -15,11 +16,31 @@ def test_calculate_taxes(prices, tax_rate, expected):
 
 
 def test_calculate_taxes_invalid_tax_rate():
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(ValueError):
         calculate_taxes([110, 220, 330], -10)
-    assert str(exc_info.value) == 'Неверный налоговый процент'
 
 def test_calculate_taxes_invalid_price():
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(ValueError):
         calculate_taxes([0, -20], 33)
-    assert str(exc_info.value) == 'Неверная цена'
+
+@pytest.mark.parametrize('price, tax_rate, expected', [(100, 10, 110.0), (50, 5, 52.5)])
+def test_calculate_tax(price, tax_rate, expected):
+    assert calculate_tax(price, tax_rate) == expected
+
+def test_calculate_tax_invalid_price():
+    with pytest.raises(ValueError):
+        calculate_tax(-100, 20)
+
+def test_calculate_tax_invalid_tax_rate():
+    with pytest.raises(ValueError) as exc:
+        calculate_tax(100, -5)
+    assert str(exc.value) == 'Неверный налоговый процент'
+
+def test_calculate_tax_invalid_tax_rate():
+    with pytest.raises(ValueError) as exc:
+        calculate_tax(100, 100)
+
+
+
+
+
